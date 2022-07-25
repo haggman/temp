d83491d7f191795bad4714f1609aa5e7cba99ca9
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-25 16:28:34 +0100

    Add sed on table name

--- gogo.sh
+++ gogo.sh
@@ -1,7 +1,7 @@
-bkt=
-bkt=""
+bkt="name"
 instanceId="starships"
-databaseId="shipbase"
+table_name="shipbase"
 sed -i "s/bucketname/$bkt/" startrek.json
+sed -i "s/Ships/$table_name/" startrek.json
 gsutil cp startrek.json "gs://$bkt/"
 gcloud dataflow jobs run in-to-spanner --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_Cloud_Spanner --region us-central1 --staging-location gs://$bkt/temp --parameters instanceId=$instanceId,databaseId=$databaseId,importManifest=gs://$bkt/startrek.json
