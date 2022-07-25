574ff3b6e0a135ad211adde5e4037b9dd3eafca6
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-25 16:39:10 +0100

    Forgot DB Name

--- gogo.sh
+++ gogo.sh
@@ -1,7 +1,8 @@
 bkt="name"
 instanceId="starships"
 table_name="shipbase"
+database_name="starbase"
 sed -i "s/bucketname/$bkt/" startrek.json
 sed -i "s/Ships/$table_name/" startrek.json
 gsutil cp startrek.json "gs://$bkt/"
-gcloud dataflow jobs run in-to-spanner --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_Cloud_Spanner --region us-central1 --staging-location gs://$bkt/temp --parameters instanceId=$instanceId,databaseId=$databaseId,importManifest=gs://$bkt/startrek.json
+gcloud dataflow jobs run in-to-spanner --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_Cloud_Spanner --region us-central1 --staging-location gs://$bkt/temp --parameters instanceId=$instanceId,databaseId=$database_name,importManifest=gs://$bkt/startrek.json
