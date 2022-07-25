8fd94a4550086e87a57e8f0c5e171c774d7278d0
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-25 16:17:42 +0100

    Update gogo.sh

--- gogo.sh
+++ gogo.sh
@@ -1,6 +1,7 @@
 bkt=
-instanceId=
-databaseId=
+bkt=""
+instanceId="starships"
+databaseId="shipbase"
 sed -i "s/bucketname/$bkt/" startrek.json
 gsutil cp startrek.json "gs://$bkt/"
 gcloud dataflow jobs run in-to-spanner --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_Cloud_Spanner --region us-central1 --staging-location gs://$bkt/temp --parameters instanceId=$instanceId,databaseId=$databaseId,importManifest=gs://$bkt/startrek.json
