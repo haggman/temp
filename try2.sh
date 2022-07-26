956847984d796a86af636e574b0662e05e62045c
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-26 10:58:00 +0100

    Delete try2.sh

--- try2.sh
+++
@@ -1,4 +0,0 @@
-bkt=
-instanceId=
-databaseId=
-gcloud dataflow jobs run in-to-spanner --gcs-location gs://dataflow-templates-us-central1/latest/GCS_Text_to_Cloud_Spanner --region us-central1 --staging-location gs://$bkt/temp --parameters instanceId=$instanceId,databaseId=$databaseId,importManifest=gs://$bkt/startrek.json
